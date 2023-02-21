e9773d22defdef14cb6c612bdb5b88549b04b358
Maxime Gouin <46935802+maximegouin@users.noreply.github.com>
2023-02-21 10:16:29 +0100

    feat: delete myself

--- src/ressources/myself.ts
+++ src/ressources/myself.ts
@@ -33,4 +33,20 @@ export class MyselfRessource extends InstanceRessource {
             throw handleError(error);
         }
     }
+
+    /**
+     * User wants to delete it's account
+     * Can't login after this
+     * Send email to customer care notifying user want to delete it's account
+     * @param payload
+     * @returns
+     */
+    async delete() {
+        try {
+            const { data } = await this.instance.delete('users/user/myself');
+            return data;
+        } catch (error) {
+            throw handleError(error);
+        }
+    }
 }
