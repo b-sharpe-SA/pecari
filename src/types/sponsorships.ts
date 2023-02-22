45e7bd678a49a03274156fb88524f12206cc66f8
Maxime Gouin <46935802+maximegouin@users.noreply.github.com>
2023-02-22 10:06:49 +0100

    fix(sponsorship): main type

--- src/types/sponsorships.ts
+++ src/types/sponsorships.ts
@@ -1,9 +1,19 @@
-import { Customer } from './customer';
+import { CustomerNextStep } from './customer';
 
-export interface Sponsorship extends Customer {
+export interface Sponsorship {
     first_activation_date: string;
     first_transaction_date: string;
     creation_datetime: string;
+    name: string;
+    next_step: CustomerNextStep;
+    pending: true;
+    rewards: [
+        {
+            creation_datetime: string;
+            amount: string;
+            fees_processing: string;
+        }
+    ];
 }
 
 export interface SponsorshipInfoByCode {
