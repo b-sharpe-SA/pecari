2d6771ce2465c7998e65f0209e99baaaf8e5872b
Maxime Gouin <46935802+maximegouin@users.noreply.github.com>
2024-03-21 16:44:40 +0100

    include users ressource

--- src/ressources/management/index.ts
+++ src/ressources/management/index.ts
@@ -1,6 +1,9 @@
 import { InstanceRessource } from '@helpers';
 import { TokenRessource } from './token';
+import { UsersRessource } from './users';
 
 export class ManagementRessource extends InstanceRessource {
     token = new TokenRessource(this);
+
+    users = new UsersRessource(this);
 }
