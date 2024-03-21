2681c29fa0cd6284522db164b646249aa19c82a2
Maxime Gouin <46935802+maximegouin@users.noreply.github.com>
2024-03-21 16:22:00 +0100

    remove type module from package.json

--- dist/constants/config.js
+++ dist/constants/config.js
@@ -3,5 +3,5 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.MANAGEMENT_PATH = exports.PUBLIC_PATH = exports.LANGUAGE_HEADER_KEY = exports.AUTH_HEADER_KEY = void 0;
 exports.AUTH_HEADER_KEY = 'Authorization';
 exports.LANGUAGE_HEADER_KEY = 'Accept-Language';
-exports.PUBLIC_PATH = "/api/v2";
-exports.MANAGEMENT_PATH = "/management";
+exports.PUBLIC_PATH = '/api/v2';
+exports.MANAGEMENT_PATH = '/management';
