0e9f2a59150da61a4262a5c0e8f7e451a8736285
Maxime Gouin <46935802+maximegouin@users.noreply.github.com>
2024-03-21 16:44:12 +0100

    users crud
    include users ressource
    
    fix users type
    
    seperate type for ordering
    
    feat: create user
    
    build

--- dist/ressources/management/index.d.ts
+++ dist/ressources/management/index.d.ts
@@ -1,5 +1,7 @@
 import { InstanceRessource } from '../../helpers';
 import { TokenRessource } from './token';
+import { UsersRessource } from './users';
 export declare class ManagementRessource extends InstanceRessource {
     token: TokenRessource;
+    users: UsersRessource;
 }
