2ce7c77c6bd1b4f426e4c60be294b7a72b0bb964
Maxime Gouin <46935802+maximegouin@users.noreply.github.com>
2024-10-01 15:57:54 +0200

    front end data with - currencyDocumentsList

--- src/types/customer.ts
+++ src/types/customer.ts
@@ -102,6 +102,7 @@ export interface CustomerFrontEndData extends Record<string, any> {
     accountType: 'own' | 'salary';
     ibanDocumentDownloaded: boolean;
     employerDocumentDownloaded: boolean;
+    currencyDocumentsList: string[];
 }
 
 export type UpdateCustomerPayload = Partial<Omit<Customer, 'reference'>>;
