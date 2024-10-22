f21986ff492303c7fcb5884287f7da0498c48489
Hugo <cclementehugo@gmail.com>
2024-10-22 14:19:01 +0200

    update return type

--- dist/ressources/customer.d.ts
+++ dist/ressources/customer.d.ts
@@ -25,5 +25,5 @@ export declare class CustomerRessource extends InstanceRessource {
      * @param reference customer reference
      * @param flags array of flags to validate
      */
-    validateFlags(reference: string, flags: FieldsWithValidationFlags[]): Promise<any>;
+    validateFlags(reference: string, flags: FieldsWithValidationFlags[]): Promise<Customer>;
 }
